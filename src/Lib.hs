ceb4914ba4e38babc8506077e5467465f677d45c
magnunm <magnusnym97@gmail.com>
2023-03-06 23:07:47 +0100

    Solver for the Pirate Riddle with N pirates

---
+++ src/Lib.hs
@@ -0,0 +1,54 @@
+module Lib
+    ( distribution
+    ) where
+
+import           Data.List (sortBy)
+
+-- | Nash equilibrium (optimal) distribution of coins to `n` pirates.
+--
+-- Including the Captain. Argument `n` is a non-zero integer.
+--
+-- TODO: Handle when `100` is too few coins to distribute.
+distribution :: Int -> [Int]
+distribution 1 = [100]
+distribution n =
+  (100 - sum distributionTail) : distributionTail
+  where distributionTail = distributionForPirates (distribution (n - 1))
+
+-- | The distribution the Captain should suggest for his subordinate pirates.
+--
+-- Given the Nash equilibrium distribution between said pirates if the Captain
+-- was killed.
+distributionForPirates :: (Integral a) => [a] -> [a]
+distributionForPirates prevDist =
+  let yays = neededYays (length prevDist)
+      piratesByCost = piratesByMinForYay prevDist
+  in
+    map snd $ sortByFst $ mapFromIndex (\(i, _) -> (i, 0)) yays piratesByCost
+
+-- | Sort the non-captain pirates by coins required for yay.
+--
+-- From lowest to highest minimum required for a yay. Returned together with
+-- their index from the captain, starting at 1 for the first pirate after the
+-- captain. Takes the Nash equilibrium distribution of coins if the captain
+-- is killed as argument.
+piratesByMinForYay :: (Integral a, Integral b) => [a] -> [(b, a)]
+piratesByMinForYay prevDist =
+  sortBy (\(_, x) (_, y) -> compare x y) (zip [1..] (minForYay prevDist))
+
+-- | Yay's needed from non-captain pirates to pass the vote.
+--
+-- Given the number of pirates excluding the captain.
+neededYays :: (Integral a) => a -> a
+neededYays numPirates =
+  quot piratesAndCaptain 2 - if rem piratesAndCaptain 2 == 0 then 1 else 0
+  where piratesAndCaptain = numPirates + 1
+
+minForYay :: (Num a) => [a] -> [a]
+minForYay = map (+ 1)
+
+mapFromIndex :: (a -> a) -> Int -> [a] -> [a]
+mapFromIndex func index xs = (take index xs) ++ (map func (drop index xs))
+
+sortByFst :: (Integral a) => [(a, b)] -> [(a, b)]
+sortByFst = sortBy (\(x, _) (y, _) -> compare x y)
