b5d43416ac39910203a0da93bc0427a27d540aaa
magnunm <magnusnym97@gmail.com>
2023-03-11 19:35:39 +0100

    Take number of pirates as command line argument

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,26 @@
 module Main (main) where
 
+import           System.Environment (getArgs)
+import           System.Exit        (ExitCode (ExitFailure, ExitSuccess),
+                                     exitWith)
+import           Text.Read          (readMaybe)
+
 import           Lib
 
 main :: IO ()
-main = print (distribution 5)
+main = getArgs >>= parseArgs >>= print . distribution >> exitSuccess
+
+parseArgs :: [String] -> IO Int
+parseArgs [] = putStrLn "Number of pirates required" >> usage >> exitFail
+parseArgs (x:_) = case x of
+  "-h"     -> usage >> exitSuccess
+  "--help" -> usage >> exitSuccess
+  _        -> case (readMaybe x :: Maybe Int) of
+    Nothing -> putStrLn "Number of pirates must be an integer" >> usage >> exitFail
+    Just numPirates -> pure numPirates
+
+usage = putStrLn "Usage: pirate-riddle [integer number of pirates]"
+
+exitFail = exitWith (ExitFailure 1)
+
+exitSuccess = exitWith ExitSuccess
